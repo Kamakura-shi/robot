5d50112d05040f307d3ea6babadeabbe5fa1b814
Hani Ouali <116925402+Kamakura-shi@users.noreply.github.com>
2025-09-28 10:32:15 -0400

    Update GPIO.c

--- src/GPIO.c
+++ src/GPIO.c
@@ -1,3 +1,15 @@
+/****************************Proto 1********************************************
+ Author : Hani Ouali, Alexandre Boldron & Léo Alagheband
+ Date   : 09/17/2025
+ Description :
+ GPIO : -Registre d’activation des horloges de périphériques : RCC-> AHBENR [32 AHBENR [32:0] (32 bits)
+        -Registre de mode du port E/S : GPIOA-> MODER [32 MODER [32:0] (32 bits)
+        -Registre de type de sortie du port E/S : GPIOA-> OTYPER [32 OTYPER [32:0] (32 bits)
+        -Registre de mode Pull-up/Pull-down du port E/S : GPIOA-> PUPDR [32 PUPDR [32:0]
+        -Registre de vitesse du port E/S : GPIOA-> OSPEEDR [32 OSPEEDR [32:0] (32 bits)
+        -Registre « LOW » de fonctions alternatives du port E/S : GPIOA-> AFRL[32 AFRL[32:0] (32 bits)
+*/
+
 #include "GPIO.h"
 
 void Configure_GPIO(void) {
